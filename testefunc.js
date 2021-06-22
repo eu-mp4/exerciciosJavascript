d58ff10726098010a40f01c0c2363f7429981125
marcellacg <marcelladacosta@gmail.com>
2021-06-22 18:00:44 -0300

    teste de javascript

---
+++ testefunc.js
@@ -0,0 +1,20 @@
+/*function teste() {
+    person = {name:"John", age:31, city:"New York"};
+    console.log(person['name']);
+}
+
+teste()*/
+
+//const teste = { 
+    /*isto é um OBJETO por isso que tem que ser declarado em uma variável; não confundir com JSON, 
+    onde as chaves TÊM que ter aspas, apesar de também funcionar*/
+//    saudacao: 'bom dia',
+//    falar () {
+//        console.log(this.saudacao)
+//    }
+//}
+
+//teste.falar()
+//ou
+//const teste1 = teste.falar.bind(teste)
+//teste1()
