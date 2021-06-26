6fb812d4f8b2e4039c0e49cd17d8d7b566d83d53
marcellacg <marcelladacosta@gmail.com>
2021-06-26 00:07:04 -0300

    comitado

---
+++ Exercícios_Javascript.js
@@ -0,0 +1,72 @@
+/*function testes(num1, num2) {
+    console.log(`Soma: ${num1+num2}\nSubtração: ${num1-num2}\nMultiplicação: ${num1*num2}\nDivisão: ${num1/num2}`)
+
+}
+
+testes(10, 2)*/
+
+/*function triangulos(lado1, lado2, lado3) {
+    if (lado1 == 0 || lado2 == 0 || lado3 == 0) {
+        console.log('Não é possível fazer triangulo')
+    }
+    else if (lado1 != lado2 && lado2 != lado3 && lado3 != lado1) {
+        console.log('Triangulo escaleno');
+    }
+    else if (lado1 == lado2 && lado1 != lado3 || lado2 == lado3 && lado2 != lado1 || lado3 == lado1 && lado3 != lado2) {
+        console.log('Triangulo isósceles');
+    }
+    else {
+        console.log('Triangulo equilátero');
+    }
+}
+
+triangulos(4, 2, 2)*/
+
+/*function basis(base, expoente) {
+    return base**expoente
+}
+
+console.log(basis(5, 2))*/
+
+/*function retornado(dividendo, divisor) {
+    console.log(dividendo/divisor)
+    console.log(`O resto da divisão é ${dividendo%divisor}`)
+}
+
+retornado(12, 5)*/
+
+/*function chame() {
+    let valor = 0.1 + 0.2;
+    let valorFormatado = valor.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'});
+    console.log(valorFormatado)
+}
+
+chame()*/
+
+/*function jurosCompostos(capital, taxa, tempo) {
+    const montante = capital*(1+taxa)**tempo
+    console.log(Math.round(montante))
+}
+
+function jurosSimples(capital, juros) {
+    let mont = capital+juros
+    console.log(mont)
+}
+
+jurosCompostos(100, 7, 12)
+jurosSimples(1400, 6)*/
+
+/*10
+
+function divisivel(num, tres = 3) {
+    if (num%tres == 0) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+console.log(divisivel(15))*/
+
+
